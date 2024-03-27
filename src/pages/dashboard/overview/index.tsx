15710fe211a4a206a05cec06395bcbb417a810dd
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2024-03-27 16:19:01 -0400

    create card with detailed infos

--- src/pages/dashboard/overview/index.tsx
+++ src/pages/dashboard/overview/index.tsx
@@ -1,26 +1,52 @@
+import styles from './styles.module.scss'
 import PresentationChart from '@components/charts/presentation-chart'
 import Dropzone from '@components/dropzone'
 import { Button } from '@components/ui/button'
+import { CommandSeparator } from '@components/ui/command'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@components/ui/tabs'
+import { twMerge } from 'tailwind-merge'
 
 export default function Overview() {
   return (
-    <section className="h-full w-full">
+    <section className={styles.full}>
       <Header />
-      <Tabs defaultValue="overview" className="my-3 h-full w-full">
+      <Tabs defaultValue="overview" className={twMerge(styles.full, 'my-3')}>
         <TabsList>
           <TabsTrigger value="overview">Visão geral</TabsTrigger>
           <TabsTrigger value="commercials">Propagandas</TabsTrigger>
         </TabsList>
         <TabsContent value="overview">
-          <div className="grid w-full grid-cols-1 grid-rows-2 gap-y-8">
-            <div className="flex flex-grow gap-4">
+          <div className={styles.overviewGrid}>
+            <div className={styles.dropzoneContainer}>
               <Dropzone status="toUpload" />
               <Dropzone status="blocked" />
               <Dropzone status="blocked" />
             </div>
-            <div className="h-[333px]">
+            <div className={styles.statisticsContainer}>
               <PresentationChart />
+              <div className={styles.card}>
+                <h1 className={styles.title}>Total de propagandas:</h1>
+                <span className={styles.emphasis}>1</span>
+                <div className={twMerge(styles.separator, 'my-2')} />
+                <div className="flex flex-col gap-4">
+                  <div>
+                    <h2 className={styles.subtitle}>Resumo</h2>
+                    <span className={styles.description}>Últimos 30 dias</span>
+                  </div>
+                  <div className="flex flex-col gap-4">
+                    <div className={styles.lineDetails}>
+                      <h1>Tempo de exibição (horas)</h1>
+                      <div className={twMerge(styles.separator, 'flex-1')} />
+                      <span>14h -</span>
+                    </div>
+                    <div className={styles.lineDetails}>
+                      <h1>Vezes apresentadas no dia</h1>
+                      <div className={twMerge(styles.separator, 'flex-1')} />
+                      <span>43 -</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </TabsContent>
